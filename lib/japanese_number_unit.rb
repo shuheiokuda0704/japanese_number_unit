9719ceaf0e352cd2916958e5b3fa9de10176c1d7
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2019-12-18 18:10:07 +0900

    Implement

--- lib/japanese_number_unit.rb
+++ lib/japanese_number_unit.rb
@@ -2,5 +2,14 @@ require "japanese_number_unit/version"
 
 module JapaneseNumberUnit
   class Error < StandardError; end
-  # Your code goes here...
+end
+
+class Integer
+  def 十; self * 10; end
+  def 百; self * 100; end
+  def 千; self * 1000; end
+  def 万; self * 10000; end
+  def 億; self * 1_0000_0000; end
+  def 兆; self * 1_0000_0000_0000; end
+  def 京; self * 1_0000_0000_0000_0000; end
 end
--- spec/japanese_number_unit_spec.rb
+++ spec/japanese_number_unit_spec.rb
@@ -3,7 +3,31 @@ RSpec.describe JapaneseNumberUnit do
     expect(JapaneseNumberUnit::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "convert japanese number unit to integer by 1" do
+    expect(1.十).to eq(10)
+    expect(1.百).to eq(100)
+    expect(1.千).to eq(1000)
+    expect(1.万).to eq(10000)
+    expect(1.億).to eq(1_0000_0000)
+    expect(1.兆).to eq(1_0000_0000_0000)
+    expect(1.京).to eq(1_0000_0000_0000_0000)
   end
+
+  it "convert japanese number unit to integer by 9" do
+    expect(9.十).to eq(90)
+    expect(99.百).to eq(9900)
+    expect(999.千).to eq(999000)
+    expect(9999.万).to eq(99990000)
+    expect(9.億).to eq(9_0000_0000)
+    expect(99.兆).to eq(99_0000_0000_0000)
+    expect(999.京).to eq(999_0000_0000_0000_0000)
+  end
+
+  it "convert japanese number unit to integer by method chain" do
+    expect(1.十.万).to eq(10_0000)
+    expect(1.百.億).to eq(100_0000_0000)
+    expect(1.千.兆).to eq(1000_0000_0000_0000)
+    expect(1.十.京).to eq(10_0000_0000_0000_0000)
+  end
+
 end
